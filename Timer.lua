43f1d6c40daeadabc7e54126f95291b9b968497d
BrianTheBrain11 <68790720+BrianTheBrain11@users.noreply.github.com>
2025-07-27 14:07:08 -0700

    Extended the shittyass timer table
    Extended this chatgpt ass generated timer library with some more functions that i will definitely probably never end up using nor will anyone else use these function extensions but it will put my pfp in a little commit bubble on github. I take no responsibility if this commit completely breaks the functionality.

--- Timer.lua
+++ Timer.lua
@@ -20,10 +20,20 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 
+
+---@class Timer The timer utility class. Should use DCS's built in update functionality. 
+
 local Timer = {}
 Timer.__index = Timer
 
-function Timer:new(duration, updateRate, callback)
+---Create a new instance of the Timer table
+---@param duration number Default 0.05 seconds. The duration set for the timer.
+---@param updateRate number Default 1 second. The time step incremented every update
+---@param callback function The callback function to execute when the timer goes off.
+---@param persistentRinging boolean Default false. Whether or not the timer should execute the callback every update after it goes off. True means it should. 
+---@param autoReset boolean Default false. Should the timer automatically reset itself once it has finished?
+---@return Timer self The timer table that has been created.
+function Timer:new(duration, updateRate, callback, persistentRinging, autoReset)
     local self = setmetatable({}, Timer)
     self.duration = duration or 1
     self.updateRate = updateRate or 0.05
@@ -31,42 +41,64 @@ function Timer:new(duration, updateRate, callback)
     self.running = false
     self.elapsed = 0
     self.completed = false
+    self.persistentRinging = persistentRinging or false
+    self.autoRest = autoReset or false
+    assert(not (persistentRinging and autoReset), "The timer classes persistentRinging and autoReset should not both be true")
     return self
 end
 
+---Starts the timer. 
 function Timer:startTimer()
     self.running = true
     self.elapsed = 0
     self.completed = false
 end
 
+---Will stop the timer prematuretly, or end the "ringing" if persistentRinging is true.
 function Timer:stopTimer()
     self.running = false
 end
 
+---Resets the timer. Can be used to reset the timer before its finished, or to clear it after. 
 function Timer:resetTimer()
     self.elapsed = 0
     self.completed = false
     self.running = false
 end
 
+---Update the timer for its time step. Should be called in the DCS device update function. 
 function Timer:updateTimer()
-    if not self.running or self.completed then return end
+    if not self.running or (self.completed and not self.persistentRinging) then return end
 
-    self.elapsed = self.elapsed + self.updateRate
+    if self.running then
+        self.elapsed = self.elapsed + self.updateRate  
+    end
+    
     if self.elapsed >= self.duration then
+
         self.running = false
         self.completed = true
+
         if self.callback then
             self.callback()
+        end
+
+        if self.autoReset and not self.persistentRinging then 
+            self.elapsed = 0
+            self.completed = true
+            self.running = true
         end
     end
 end
 
+---Check if the timer has finished. This is not the prefered method of use for the timer, and callbacks are encouraged to be used. However, this may be more helpful at times.
+---@return boolean self.completed Whether or not the timer has elapsed and is done counting.
 function Timer:isDone()
     return self.completed
 end
 
+---Explicitly set the timer end callback.
+---@param fn function The callback to be executed when the timer elapses.
 function Timer:setCallback(fn)
     self.callback = fn
 end
